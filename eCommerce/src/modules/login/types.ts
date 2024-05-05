d8aa273416bbddd040de0da444a452bd5256ddd9
k98940 <pv1xxb@yandex.ru>
2024-05-05 22:52:27 +0600

    feat: add types for api and state

--- eCommerce/src/modules/login/types.ts
+++ eCommerce/src/modules/login/types.ts
@@ -9,3 +9,11 @@ export type Actions = {
 export type Dispatch = {
   (action: Actions): void;
 };
+
+export type AuthResponse = {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+  scope: string;
+  refresh_token: string;
+};

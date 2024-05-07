c6e262ce90e79f92a6a77d73eb32ac9d40ddb333
yourunb <bxyura@gmail.com>
2024-05-07 14:33:52 +0300

    feat: add page sections

--- eCommerce/src/index.ts
+++ eCommerce/src/index.ts
@@ -1,1 +1,6 @@
 import './index.sass';
+
+const header = document.createElement('header');
+const main = document.createElement('main');
+const footer = document.createElement('footer');
+document.body.append(header, main, footer);

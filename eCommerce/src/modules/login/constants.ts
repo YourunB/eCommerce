056bf21a360e7be6e7717c19a71f56cebf8156aa
k98940 <pv1xxb@yandex.ru>
2024-05-09 22:51:26 +0600

    feat: edit credentials to access the project

--- eCommerce/src/modules/login/constants.ts
+++ eCommerce/src/modules/login/constants.ts
@@ -3,21 +3,11 @@ export const AUTH_BEARER = 'Bearer';
 export const PROJECT_KEY = 'tpepapuz';
 export const GRANT_PASSWORD = 'password';
 export const PATH = `/oauth/${PROJECT_KEY}/customers/token`;
-export const CLIENT_ID = 'nO6SI7A_iUtziAKsP4x6AmUz';
-export const CLIENT_SECRET = 'ijdMuAhQ0exPlJ-S61TMsVCgcfM_GtWc';
+export const CLIENT_ID = 'yK9n9gAhU-I9R9GqMuqQltBr';
+export const CLIENT_SECRET = 'OCgFOhzrouHycxT0ADKvY4s9sHA_YxHH';
 export const AUTH_PARAMS = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
 export const CONTENT_TYPE_APP = 'application/x-www-form-urlencoded';
 export const AUTH_URL = 'https://auth.eu-central-1.aws.commercetools.com';
 export const API_URL = 'https://api.eu-central-1.aws.commercetools.com';
 export const MSG_NETWORK_ERROR = '(network error) failed to fetch data from a server';
-export const SCOPES_CLIENT = [
-  'view_published_products',
-  'manage_my_payments',
-  'manage_my_shopping_lists',
-  'manage_my_quote_requests',
-  'manage_my_quotes',
-  'manage_my_orders',
-  'manage_my_business_units',
-  'view_categories',
-  'create_anonymous_token',
-];
+export const SCOPES_CLIENT = ['manage_project:tpepapuz'];

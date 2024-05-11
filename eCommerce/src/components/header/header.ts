2e1f993a1c8a55e74d92f0ca1b285b8c8d60ad44
yourunb <bxyura@gmail.com>
2024-05-11 12:10:16 +0300

    feat: add menu list

--- eCommerce/src/components/header/header.ts
+++ eCommerce/src/components/header/header.ts
@@ -11,6 +11,17 @@ logo.innerHTML = `
   <span>PLANTSTORE</span>
 `;
 
+const nav = document.createElement('nav');
+nav.classList.add('navigation');
+
+const menu = document.createElement('ul');
+menu.classList.add('menu');
+menu.innerHTML = `
+  <li>Home</li>
+  <li>Products</li>
+  <li>About</li>
+`;
+
 header.append(logo);
 
-export { header };
+export { header, menu };

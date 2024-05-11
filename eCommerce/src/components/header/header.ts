39bcdf367ef4f9690bb9f486e00b5ff12887e74f
yourunb <bxyura@gmail.com>
2024-05-11 11:56:46 +0300

    feat: add logo and shop name

--- eCommerce/src/components/header/header.ts
+++ eCommerce/src/components/header/header.ts
@@ -1,6 +1,16 @@
 import './header.sass';
+import '../../assets/images/svg/logo.svg';
 
 const header = document.createElement('header');
 header.classList.add('header');
+
+const logo = document.createElement('div');
+logo.classList.add('logo-box');
+logo.innerHTML = `
+  <img src="logo.svg">
+  <span>PLANTSTORE</span>
+`;
+
+header.append(logo);
 
 export { header };

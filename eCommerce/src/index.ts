75fa270e04c066f7adce61c0869fe06342143d87
yourunb <bxyura@gmail.com>
2024-05-11 13:43:32 +0300

    feat: add delegation menu  and add all pages for open on click menu items

--- eCommerce/src/index.ts
+++ eCommerce/src/index.ts
@@ -3,8 +3,10 @@ import './index.sass';
 import { router } from './modules/router';
 import { mainPage } from './pages/mainPage';
 import { notFoundPage } from './pages/notFoundPage';
+import { aboutPage } from './pages/aboutPage';
+import { productsPage } from './pages/productstPage';
 import { Login } from './modules/login/login';
-import { header, btnLogIn, logo } from './components/header/header';
+import { header, btnLogIn, logo, menu } from './components/header/header';
 
 const main = document.createElement('main');
 document.body.append(header, main);
@@ -16,7 +18,20 @@ router.addRoute({
     main.append(mainPage);
   },
 });
-
+router.addRoute({
+  path: '/yourunb-JSFE2023Q4/ecommerce/products',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(productsPage);
+  },
+});
+router.addRoute({
+  path: '/yourunb-JSFE2023Q4/ecommerce/about',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(aboutPage);
+  },
+});
 router.addRoute({
   path: '/yourunb-JSFE2023Q4/ecommerce/login',
   handler: () => {
@@ -24,7 +39,6 @@ router.addRoute({
     main.append(new Login().getPage());
   },
 });
-
 router.addRoute({
   path: '/yourunb-JSFE2023Q4/ecommerce/404',
   handler: () => {
@@ -51,6 +65,18 @@ logo.addEventListener('click', () => {
 btnLogIn.addEventListener('click', () => {
   router.route('/yourunb-JSFE2023Q4/ecommerce/login');
 });
-logo.addEventListener('click', () => {
-  router.route('/yourunb-JSFE2023Q4/ecommerce/');
+menu.addEventListener('click', (event) => {
+  const currentTarget = event.target as HTMLElement;
+  if (currentTarget.textContent === 'Home') {
+    router.route('/yourunb-JSFE2023Q4/ecommerce/');
+    return;
+  }
+  if (currentTarget.textContent === 'Products') {
+    router.route('/yourunb-JSFE2023Q4/ecommerce/products');
+    return;
+  }
+  if (currentTarget.textContent === 'About') {
+    router.route('/yourunb-JSFE2023Q4/ecommerce/about');
+    return;
+  }
 });

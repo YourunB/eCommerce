c804e86ed9ab46112c38e5aa0ac4c2f0fce0cb56
yourunb <bxyura@gmail.com>
2024-05-14 13:27:14 +0300

    refactor: change 404 page style and add btn home

--- eCommerce/src/pages/notFoundPage.ts
+++ eCommerce/src/pages/notFoundPage.ts
@@ -1,14 +1,21 @@
 import './notFoundPage.sass';
 import '../assets/images/svg/earth.svg';
+import { Button } from '../components/basebutton/baseButton';
+
+const btnBackHome = new Button({ textContent: 'LogIn', classNames: 'login__btn-submit' }).getElement();
+btnBackHome.textContent = 'Home';
 
 const notFoundPage = document.createElement('div');
 notFoundPage.classList.add('page-404');
-notFoundPage.innerHTML = `
-  <div>
-    <h2 class="page-404__title">404</h2>
-    <img class="page-404__image" src="earth.svg"> 
-    <p class="page-404__description">This Page Not Found</p>
-  </div>
+
+const boxNotFoundPage = document.createElement('div');
+boxNotFoundPage.innerHTML = `
+  <h2 class="page-404__title">404</h2>
+  <img class="page-404__image" src="earth.svg"> 
+  <p class="page-404__description">This Page Not Found</p>
 `;
+boxNotFoundPage.append(btnBackHome);
+
+notFoundPage.append(boxNotFoundPage);
 
 export { notFoundPage };

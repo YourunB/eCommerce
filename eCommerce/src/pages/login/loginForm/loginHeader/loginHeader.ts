325004b70b811e9af8a35db901dc686a3c4f3152
k98940 <pv1xxb@yandex.ru>
2024-05-15 23:33:02 +0600

    fix #14: remove link Register, clear inputs of form

--- eCommerce/src/pages/login/loginForm/loginHeader/loginHeader.ts
+++ eCommerce/src/pages/login/loginForm/loginHeader/loginHeader.ts
@@ -1,23 +1,11 @@
 import './loginHeader.sass';
 import { BaseComponent } from '../../../../components/baseComponent';
-import { Dispatch } from '../../../../modules/login/types';
 
 export class LoginHeader extends BaseComponent {
-  private dispatch: Dispatch;
-
-  constructor(dispatch: Dispatch) {
+  constructor() {
     super({ tagName: 'div', classNames: 'login__header' });
-    this.dispatch = dispatch;
 
     const spanLogin = new BaseComponent({ tagName: 'span', textContent: 'Login', classNames: 'header__login' });
-    const spanLine = new BaseComponent({ tagName: 'span', textContent: '|', classNames: 'header_line' });
-    const linkRegister = new BaseComponent({ tagName: 'a', textContent: 'Register', classNames: 'header_link' });
-    linkRegister.getElement().addEventListener('click', (e) => this.handleClick(e));
-    this.insertChildren([spanLogin, spanLine, linkRegister]);
-  }
-
-  private handleClick(e: Event): void {
-    e.preventDefault();
-    this.dispatch({ type: 'register', payload: { email: '', password: '' } });
+    this.insertChildren([spanLogin]);
   }
 }

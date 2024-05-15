851511fc9a62420bfc108d3db39338b0e82577fe
k98940 <pv1xxb@yandex.ru>
2024-05-15 16:19:27 +0600

    feat: add tests

--- eCommerce/src/components/helpers/__tests__/validation-rules.test.ts
+++ eCommerce/src/components/helpers/__tests__/validation-rules.test.ts
@@ -1,192 +1,192 @@
 import {
+  isToLong33,
+  isNotEmpty,
+  isPassLeast8,
+  isEnoughOlder,
+  isRightPostalCode,
   isEmailContainDog,
+  isPassContainNumber,
+  isPassContainLowercase,
+  isPassContainUppercase,
+  isNotContainWhitespaces,
   isEmailContainDomainName,
   isEmailProperlyFormatted,
-  isNotContainWhitespaces,
-  isNotEmpty,
-  isPassContainLowercase,
-  isPassContainNumber,
-  isPassContainUppercase,
-  isPassLeast8,
-  isToLong33,
+  isContainAtLeastOneLetters,
 } from '../validation-rules';
 
-describe('validation-rules: isNotEmpty', () => {
-  it('must not be empty', () => {
-    const goodData = {
-      subject: 'ddd',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: '',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isNotEmpty (must not be empty)', () => {
+  const testData = [
+    { data: { subject: 'ddd', validate: true, errors: [] }, expected: true },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isNotEmpty(goodData).validate).toBeTruthy();
-    expect(isNotEmpty(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isNotEmpty(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isNotContainWhitespaces', () => {
-  it('must not contain leading or trailing whitespace', () => {
-    const goodData = {
-      subject: 'ddd',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: ' ddd ',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isNotContainWhitespaces (must not contain leading or trailing whitespace)', () => {
+  const testData = [
+    { data: { subject: 'ddd', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'dd dd', validate: true, errors: [] }, expected: true },
+    { data: { subject: ' ddd ', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'ddd ', validate: true, errors: [] }, expected: false },
+    { data: { subject: '  ddd', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isNotContainWhitespaces(goodData).validate).toBeTruthy();
-    expect(isNotContainWhitespaces(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isNotContainWhitespaces(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isToLong33', () => {
-  it('should be less than 33 characters long', () => {
-    const goodData = {
-      subject: '123',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: '1234567890_1234567890_1234567890_1234567890',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isToLong33 (should be less than 33 characters long)', () => {
+  const testData = [
+    { data: { subject: '12345', validate: true, errors: [] }, expected: true },
+    { data: { subject: '1234567890_1234567890_1234567890_1234567890', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isToLong33(goodData).validate).toBeTruthy();
-    expect(isToLong33(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isToLong33(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isPassLeast8', () => {
-  it('must be at least 8 characters long', () => {
-    const goodData = {
-      subject: '123456789',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: '12345',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isPassLeast8 (Password must be at least 8 characters long)', () => {
+  const testData = [
+    { data: { subject: '123456789', validate: true, errors: [] }, expected: true },
+    { data: { subject: '12345', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isPassLeast8(goodData).validate).toBeTruthy();
-    expect(isPassLeast8(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isPassLeast8(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isPassContainUppercase', () => {
-  it('must contain at least one uppercase letter (A-Z)', () => {
-    const goodData = {
-      subject: 'AaaaZ',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: 'aaaaazzz',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isPassContainUppercase (must contain at least one uppercase letter (A-Z))', () => {
+  const testData = [
+    { data: { subject: 'AaaaZ', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'aaaaazzZ', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'aaaaazz', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isPassContainUppercase(goodData).validate).toBeTruthy();
-    expect(isPassContainUppercase(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isPassContainUppercase(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isPassContainLowercase', () => {
-  it('must contain at least one lowercase letter (a-z)', () => {
-    const goodData = {
-      subject: 'aZZZZZ',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: 'AAAAZZZZ',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isPassContainLowercase (must contain at least one lowercase letter (a-z))', () => {
+  const testData = [
+    { data: { subject: 'aZZZZZ', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'AAAAZZZZ', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isPassContainLowercase(goodData).validate).toBeTruthy();
-    expect(isPassContainLowercase(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isPassContainLowercase(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isPassContainNumber', () => {
-  it('must contain at least one digit (0-9)', () => {
-    const goodData = {
-      subject: '9aZZZZZ',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: 'AAAAZZZZ',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isPassContainNumber (must contain at least one digit (0-9))', () => {
+  const testData = [
+    { data: { subject: '9aZZZZZ', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'AAAAZZZZ', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isPassContainNumber(goodData).validate).toBeTruthy();
-    expect(isPassContainNumber(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isPassContainNumber(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isEmailProperlyFormatted', () => {
-  it('Email address must be properly formatted (e.g., user@example.com)', () => {
-    const goodData = {
-      subject: 'user@example.com',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: '@badtest',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isEmailProperlyFormatted (Email address must be properly formatted)', () => {
+  const testData = [
+    { data: { subject: 'user@example.com', validate: true, errors: [] }, expected: true },
+    { data: { subject: '@badtest', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'abc@badtest', validate: true, errors: [] }, expected: false },
+    { data: { subject: '@badtest.abc', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isEmailProperlyFormatted(goodData).validate).toBeTruthy();
-    expect(isEmailProperlyFormatted(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isEmailProperlyFormatted(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isEmailContainDomainName', () => {
-  it('Email address must contain a domain name (e.g., example.com)', () => {
-    const goodData = {
-      subject: 'example.com',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: 'test@badtest',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isEmailContainDomainName (Email address must contain a domain name (e.g., example.com))', () => {
+  const testData = [
+    { data: { subject: 'user@example.com', validate: true, errors: [] }, expected: true },
+    { data: { subject: '@test.abc', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'test.it', validate: true, errors: [] }, expected: true },
+    { data: { subject: '@best', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'badtest', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isEmailContainDomainName(goodData).validate).toBeTruthy();
-    expect(isEmailContainDomainName(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isEmailContainDomainName(data)).toHaveProperty('validate', expected);
   });
 });
 
-describe('validation-rules: isEmailContainDog', () => {
-  it('Email address must contain an `@` symbol separating local part and domain name', () => {
-    const goodData = {
-      subject: 'test@example.com',
-      validate: true,
-      errors: [],
-    };
-    const badData = {
-      subject: 'test.com',
-      validate: true,
-      errors: [],
-    };
+describe('validation-rules: isEmailContainDog (Email address must contain an `@` symbol separating local part and domain name)', () => {
+  const testData = [
+    { data: { subject: 'user@example.com', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'test@test.abc', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'test@best', validate: true, errors: [] }, expected: true },
+    { data: { subject: '@test.it', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'badtest@', validate: true, errors: [] }, expected: false },
+    { data: { subject: '@', validate: true, errors: [] }, expected: false },
+  ];
 
-    expect(isEmailContainDog(goodData).validate).toBeTruthy();
-    expect(isEmailContainDog(badData).validate).toBeFalsy();
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isEmailContainDog(data)).toHaveProperty('validate', expected);
+  });
+});
+
+describe('validation-rules: isEnoughOlder (User should be at least 13 years old)', () => {
+  let now = new Date();
+  now.setFullYear(now.getFullYear() - 13, now.getMonth(), now.getDate() - 2);
+  const [goodDate] = now.toISOString().split('T');
+
+  now = new Date();
+  now.setFullYear(now.getFullYear() - 13, now.getMonth(), now.getDate() + 2);
+  const [badDate] = now.toISOString().split('T');
+  const testData = [
+    { data: { subject: goodDate, validate: true, errors: [] }, expected: true },
+    { data: { subject: badDate, validate: true, errors: [] }, expected: false },
+  ];
+
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isEnoughOlder(data)).toHaveProperty('validate', expected);
+  });
+});
+
+describe('validation-rules: isRightPostalCode (Postal code must follow the format for the country (e.g., 12345))', () => {
+  const testData = [
+    { data: { subject: '12345', validate: true, errors: [] }, expected: true },
+    { data: { subject: '1234567', validate: true, errors: [] }, expected: false },
+    { data: { subject: '123', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'abc12', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'abcde', validate: true, errors: [] }, expected: false },
+    { data: { subject: '', validate: true, errors: [] }, expected: false },
+  ];
+
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isRightPostalCode(data)).toHaveProperty('validate', expected);
+  });
+});
+
+describe('validation-rules: isContainAtLeastOneLetters (Must contain at least one character a-z or A-Z)', () => {
+  const testData = [
+    { data: { subject: 'a', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'Z', validate: true, errors: [] }, expected: true },
+    { data: { subject: '123', validate: true, errors: [] }, expected: false },
+    { data: { subject: 'abc12', validate: true, errors: [] }, expected: true },
+    { data: { subject: 'abcde', validate: true, errors: [] }, expected: true },
+    { data: { subject: '#$%', validate: true, errors: [] }, expected: false },
+  ];
+
+  test.each(testData)('"$data.subject" must return "$expected"', ({ data, expected }) => {
+    expect(isContainAtLeastOneLetters(data)).toHaveProperty('validate', expected);
   });
 });

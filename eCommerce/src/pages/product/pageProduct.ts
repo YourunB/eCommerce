1475d86046f8d614610379051ed9cb26dfb3a0b8
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-05-24 14:57:57 +0300

    feat: add small slider and some styles to product page

--- eCommerce/src/pages/product/pageProduct.ts
+++ eCommerce/src/pages/product/pageProduct.ts
@@ -2,25 +2,33 @@ import { ErrorResponse, Product } from '@commercetools/platform-sdk';
 import { BaseComponent, BaseComponentProps } from '../../components/baseComponent';
 import { getProduct } from '../../modules/api/productItem';
 import './pageProduct.sass';
+import Swiper from 'swiper';
 
 export class PageProduct extends BaseComponent {
-  private productImageContainer: BaseComponent;
+  private productSwiper: BaseComponent;
   private productTextContainer: BaseComponent;
   private productPrice: BaseComponent;
   private productName: BaseComponent;
   private productDescription: BaseComponent;
-  private productImg: BaseComponent;
+  private productDescriptionName: BaseComponent;
+  private productSwiperPagination: BaseComponent;
+  private productSwiperWrapper: BaseComponent;
   constructor(props: BaseComponentProps) {
     super({ classNames: 'page-product-container', ...props });
-    this.productImageContainer = new BaseComponent({
+    this.productSwiper = new BaseComponent({
       tagName: 'div',
-      classNames: 'product-image_container',
+      classNames: 'product-swiper',
       parentNode: this.element,
     });
-    this.productImg = new BaseComponent({
-      tagName: 'img',
-      classNames: 'product-img',
-      parentNode: this.productImageContainer.getElement(),
+    this.productSwiperWrapper = new BaseComponent({
+      tagName: 'div',
+      classNames: 'product-swiper_wrapper',
+      parentNode: this.productSwiper.getElement(),
+    });
+    this.productSwiperPagination = new BaseComponent({
+      tagName: 'div',
+      classNames: 'swiper-pagination',
+      parentNode: this.productSwiper.getElement(),
     });
     this.productTextContainer = new BaseComponent({
       tagName: 'div',
@@ -37,6 +45,12 @@ export class PageProduct extends BaseComponent {
       classNames: 'product-price',
       parentNode: this.productTextContainer.getElement(),
     });
+    this.productDescriptionName = new BaseComponent({
+      tagName: 'div',
+      classNames: 'product-description-name',
+      textContent: 'Product description:',
+      parentNode: this.productTextContainer.getElement(),
+    });
     this.productDescription = new BaseComponent({
       tagName: 'div',
       classNames: 'product-description',
@@ -47,16 +61,49 @@ export class PageProduct extends BaseComponent {
       this.productName.setTextContent(data.masterData.current.name['en-GB']);
       this.productPrice.setTextContent(
         data.masterData.current.masterVariant.prices
-          ? data.masterData.current.masterVariant.prices[0].value.centAmount.toString()
+          ? `€ ${(data.masterData.current.masterVariant.prices[0].value.centAmount / 100).toLocaleString('ru-RU')}.00`
           : ''
       );
       this.productDescription.setTextContent(
         data.masterData.current.description ? data.masterData.current.description['en-GB'] : ''
       );
-      this.productImg.setAttribute({
-        name: 'src',
-        value: data.masterData.current.masterVariant.images ? data.masterData.current.masterVariant.images[0].url : '',
-      });
+      if (data.masterData.staged.masterVariant.images) {
+        const imagePromises = data.masterData.staged.masterVariant.images.map(
+          (image) =>
+            new Promise((resolve) => {
+              const productSwiperSlide = new BaseComponent({
+                tagName: 'div',
+                classNames: 'product-swiper_slide',
+                parentNode: this.productSwiperWrapper.getElement(),
+              });
+
+              const productImg = new BaseComponent({
+                tagName: 'img',
+                classNames: 'product-img',
+                parentNode: productSwiperSlide.getElement(),
+              });
+
+              productImg.setAttribute({
+                name: 'src',
+                value: image.url,
+              });
+
+              productImg.getElement().onload = resolve;
+            })
+        );
+
+        Promise.all(imagePromises).then(() => {
+          setTimeout(() => {
+            new Swiper('.product-swiper', {
+              loop: true,
+              pagination: {
+                el: '.swiper-pagination',
+              },
+            });
+          }, 0);
+        });
+      }
+
       return data;
     });
   }

b32366d96c6e37e9234a6e35486806f337323c3d
k98940 <pv1xxb@yandex.ru>
2024-05-25 17:09:52 +0600

    refactor: filterCentAmount, change query's format

--- eCommerce/src/components/helpers/filterСentAmountMin.ts
+++
@@ -1,4 +0,0 @@
-export function filterСentAmountMax(url: URL, СentAmountMax: string): URL {
-  url.searchParams.append('filter', `variants.price.centAmount: range (* to ${СentAmountMax})`);
-  return url;
-}

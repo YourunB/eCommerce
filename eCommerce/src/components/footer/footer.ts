40dd88dd39579e52baede66e74940b414bea2b17
yourunb <bxyura@gmail.com>
2024-05-27 09:41:52 +0300

    refactor: change footer style

--- eCommerce/src/components/footer/footer.ts
+++ eCommerce/src/components/footer/footer.ts
@@ -6,7 +6,7 @@ footer.classList.add('footer');
 
 footer.innerHTML = `
   <span>2024</span>
-  <a class="footer__link" target="_blank" rel="noopener" href="https://rs.school/">&copy; RS School <img class="school-logo" src="logo-rs.svg" alt="Logo"></a>
+  <a class="footer__link" target="_blank" rel="noopener" href="https://rs.school/">&copy; RSSchool <img class="school-logo" src="logo-rs.svg" alt="Logo"></a>
 `;
 
 export { footer };

48e9c2ba2c68249ca4dc3c51c682d38c8a725ad8
Nadzeya Tsimashenkava <timoshenkova.nadya@gmail.com>
2024-05-29 13:21:49 +0300

    refactor: improve functions structure

--- eCommerce/src/pages/product/pageProduct.ts
+++ eCommerce/src/pages/product/pageProduct.ts
@@ -30,6 +30,7 @@ export class PageProduct extends BaseComponent {
       parentNode: this.element,
     });
     this.overlay.getElement().addEventListener('click', () => this.switchModal(false));
+
     this.modalSwiper = new BaseComponent({
       tagName: 'div',
       classNames: 'modal-swiper',
@@ -41,6 +42,7 @@ export class PageProduct extends BaseComponent {
       parentNode: this.modalSwiper.getElement(),
     });
     this.closeModalButton.getElement().addEventListener('click', () => this.switchModal(false));
+
     this.productSwiper = new BaseComponent({
       tagName: 'div',
       classNames: 'swiper',
@@ -51,7 +53,6 @@ export class PageProduct extends BaseComponent {
       classNames: 'swiper-wrapper',
       parentNode: this.productSwiper.getElement(),
     });
-
     this.productSwiperButtonPrev = new BaseComponent({
       tagName: 'div',
       classNames: 'swiper-button-prev',
@@ -62,6 +63,7 @@ export class PageProduct extends BaseComponent {
       classNames: 'swiper-button-next',
       parentNode: this.productSwiper.getElement(),
     });
+
     this.productTextContainer = new BaseComponent({
       tagName: 'div',
       classNames: 'product-text_container',
@@ -88,75 +90,26 @@ export class PageProduct extends BaseComponent {
       classNames: 'product-description',
       parentNode: this.productTextContainer.getElement(),
     });
+
     const Id = window.location.hash.substring(1);
+
     getProduct(Id).then((data: Product | ErrorResponse) => {
       if ('statusCode' in data) return;
       const product = data.masterData;
       this.productName.setTextContent(product.current.name['en-GB']);
-      if (product.staged.masterVariant.prices) {
-        const currentPrice =
-          product.staged.masterVariant.prices[0].discounted?.value.centAmount ||
-          product.staged.masterVariant.prices[0].value.centAmount;
-        const actualPrice = new BaseComponent({
-          tagName: 'span',
-          textContent: `€${(currentPrice / 100).toLocaleString('ru-RU', { minimumFractionDigits: 2 })}`,
-          classNames: 'actual-price',
-        });
-        this.productPrice.insertChild(actualPrice);
-        if (product.staged.masterVariant.prices[0].discounted) {
-          const oldPrice = new BaseComponent({
-            tagName: 'span',
-            textContent: `€${(product.staged.masterVariant.prices[0].value.centAmount / 100).toLocaleString('ru-RU', { minimumFractionDigits: 2 })}`,
-            classNames: 'old-price',
-          });
-          this.productPrice.insertChild(oldPrice);
-        }
-      }
-
-      // this.productPrice.setTextContent(
-      //   product.current.masterVariant.prices
-      //     ? `€ ${(product.current.masterVariant.prices[0].value.centAmount / 100).toLocaleString('ru-RU', { minimumFractionDigits: 2 })}`
-      //     : ''
-      // );
-      // if ('discounted' in data) {
-      //   const newPrice = new BaseComponent({
-      //     tagName: 'span',
-      //     textContent: product.current.masterVariant.prices
-      //       ? `€ ${(product.current.masterVariant.prices[0].discounted?.value.centAmount / 100).toLocaleString('ru-RU', { minimumFractionDigits: 2 })}`
-      //       : '',
-      //     classNames: 'price-container__old-price',
-      //   });
-      //   this.productPrice.insertChild(newPrice);
-      // }
       this.productDescription.setTextContent(product.current.description ? product.current.description['en-GB'] : '');
+      this.renderPrices(product);
       if (product.staged.masterVariant.images) {
-        product.staged.masterVariant.images.forEach((image) => {
-          const productSwiperSlide = new BaseComponent({
-            tagName: 'div',
-            classNames: 'swiper-slide',
-            parentNode: this.productSwiperWrapper.getElement(),
-          });
-
-          const productImg = new BaseComponent({
-            tagName: 'img',
-            classNames: 'product-img',
-            parentNode: productSwiperSlide.getElement(),
-          });
-
-          productImg.setAttribute({
-            name: 'src',
-            value: image.url,
-          });
-        });
+        this.renderImages(product);
         this.renderSwiper();
       }
 
       return data;
     });
   }
+
   renderSwiper() {
-    console.log(JSON.stringify(document.querySelector('.swiper')));
-    const swiper = new Swiper('.swiper', {
+    new Swiper('.swiper', {
       slidesPerView: 1,
       centeredSlides: true,
       modules: [Navigation],
@@ -165,19 +118,15 @@ export class PageProduct extends BaseComponent {
         prevEl: '.swiper-button-prev',
       },
     });
-
     const slides = this.productSwiperWrapper.getChildren();
     slides.forEach((slide, index) => {
       const [image] = slide.children;
       image.addEventListener('click', () => {
         this.switchModal(true, index);
-        console.log(slides);
-        console.log(index);
       });
     });
-
-    console.log(swiper);
   }
+
   switchModal = (isOpen: boolean, index?: number) => {
     if (isOpen) {
       this.modalSwiper.setClassName('modal-swiper_active');
@@ -185,7 +134,7 @@ export class PageProduct extends BaseComponent {
       const modal = this.productSwiper.getElement().cloneNode(true);
       (modal as HTMLElement).classList.add('slider-enlarge');
       this.modalSwiper.getElement().appendChild(modal);
-      const swiper = new Swiper('.swiper', {
+      new Swiper('.swiper', {
         slidesPerView: 1,
         initialSlide: index,
         centeredSlides: true,
@@ -195,7 +144,6 @@ export class PageProduct extends BaseComponent {
           prevEl: '.swiper-button-prev',
         },
       });
-      console.log(swiper);
     }
     if (!isOpen) {
       const modalChildren = this.modalSwiper.getElement().children;
@@ -206,5 +154,48 @@ export class PageProduct extends BaseComponent {
       this.modalSwiper.removeClassName('modal-swiper_active');
       this.overlay.removeClassName('overlay_active');
     }
+  };
+
+  renderPrices = (product: Product['masterData']) => {
+    if (product.staged.masterVariant.prices) {
+      const currentPrice =
+        product.staged.masterVariant.prices[0].discounted?.value.centAmount ||
+        product.staged.masterVariant.prices[0].value.centAmount;
+      const actualPrice = new BaseComponent({
+        tagName: 'span',
+        textContent: `€${(currentPrice / 100).toLocaleString('ru-RU', { minimumFractionDigits: 2 })}`,
+        classNames: 'actual-price',
+      });
+      this.productPrice.insertChild(actualPrice);
+      if (product.staged.masterVariant.prices[0].discounted) {
+        const oldPrice = new BaseComponent({
+          tagName: 'span',
+          textContent: `€${(product.staged.masterVariant.prices[0].value.centAmount / 100).toLocaleString('ru-RU', { minimumFractionDigits: 2 })}`,
+          classNames: 'old-price',
+        });
+        this.productPrice.insertChild(oldPrice);
+      }
+    }
+  };
+
+  renderImages = (product: Product['masterData']) => {
+    product.staged.masterVariant.images?.forEach((image) => {
+      const productSwiperSlide = new BaseComponent({
+        tagName: 'div',
+        classNames: 'swiper-slide',
+        parentNode: this.productSwiperWrapper.getElement(),
+      });
+
+      const productImg = new BaseComponent({
+        tagName: 'img',
+        classNames: 'product-img',
+        parentNode: productSwiperSlide.getElement(),
+      });
+
+      productImg.setAttribute({
+        name: 'src',
+        value: image.url,
+      });
+    });
   };
 }

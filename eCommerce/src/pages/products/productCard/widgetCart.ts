7a4f980d70cc9f5240b5077d26c98f39a4273a4e
Konstantin <88248839+K98940@users.noreply.github.com>
2024-06-10 13:07:55 +0600

    feat: implement a loading indicator while the API call is in progress (#52)

--- eCommerce/src/pages/products/productCard/widgetCart.ts
+++ eCommerce/src/pages/products/productCard/widgetCart.ts
@@ -1,18 +1,23 @@
 import { BaseComponent } from '../../../components/baseComponent';
 import { Button } from '../../../components/basebutton/baseButton';
 import { DispatchProducts } from '../../../modules/products/types';
+import { ProductsStateLine } from '../../../modules/products/productsState';
+import { isLineItem } from '../../../components/helpers/predicates';
 
 export class WidgetCart extends BaseComponent {
   private dispatch: DispatchProducts;
   private quantity: number;
   private cartButton: Button;
   private cartText: BaseComponent;
+  private localState: ProductsStateLine;
+  private spinner: BaseComponent | undefined;
 
   constructor(quantity: number, productId: string, dispatch: DispatchProducts) {
     super({ tagName: 'section', classNames: 'widget-cart__container' });
     this.quantity = quantity;
     this.dispatch = dispatch;
     this.setAttributeIsInCart(this.quantity);
+    this.localState = 'idle';
 
     this.cartText = new BaseComponent({
       tagName: 'span',
@@ -24,23 +29,52 @@ export class WidgetCart extends BaseComponent {
     this.cartButton.getElement().addEventListener('click', (e: Event) => {
       e.preventDefault();
       e.stopPropagation();
-      if (this.quantity > 0) return;
+      if (this.localState === 'isFetching') return;
+      this.localState = 'isFetching';
+      if (this.quantity > 0) {
+        this.localState = 'idle';
+        return;
+      }
+      this.cartButton.setClassName('widget-cart__button_is-fetching');
+      this.spinner = this.setSpinner(this);
       this.dispatch({ type: 'cart-addLine', payload: { prop1: productId, prop2: '' } });
     });
     this.insertChildren([this.cartButton]);
     if (this.quantity) this.insertChildren([this.cartText]);
   }
 
-  public update(quantity: number): void {
-    if (quantity === 1) this.insertChildren([this.cartText]);
-    this.setAttributeIsInCart(quantity);
-    this.cartText.setTextContent(`${quantity}`);
-    this.quantity = quantity;
+  public update(stateLine: ProductsStateLine): void {
+    this.cartButton.removeClassName('widget-cart__button_is-fetching');
+    this.localState = 'idle';
+    if (this.spinner) this.deleteSpinner(this.spinner);
+
+    if (isLineItem(stateLine)) {
+      const { quantity } = stateLine;
+      if (quantity === 1) this.insertChildren([this.cartText]);
+      this.setAttributeIsInCart(quantity);
+      this.cartText.setTextContent(`${quantity}`);
+      this.quantity = quantity;
+    }
   }
 
   private setAttributeIsInCart(quantity: number): void {
     if (quantity > 0) {
+      /* If a product is added to the cart, it will have the attribute 'data-set-is-in-cart' and change its style*/
       this.setAttribute({ name: 'data-set-is-in-cart', value: '' });
     } else this.removeAttribute({ name: 'data-set-is-in-cart' });
+  }
+
+  private setSpinner(container: BaseComponent): BaseComponent {
+    const spinnerContainer = new BaseComponent({ tagName: 'div', classNames: 'spinner__container' });
+
+    container.insertChild(spinnerContainer);
+    return spinnerContainer;
+  }
+
+  private deleteSpinner(spinner: BaseComponent): void {
+    setTimeout(() => {
+      if (this.spinner) spinner.destroy();
+      this.spinner = undefined;
+    }, 1800);
   }
 }

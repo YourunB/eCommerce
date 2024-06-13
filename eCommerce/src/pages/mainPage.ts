6ea11db8133bddab403296026aa7efc88adaabe5
yourunb <bxyura@gmail.com>
2024-06-13 15:13:14 +0300

    feat: add image to discount on main page

--- eCommerce/src/pages/mainPage.ts
+++ eCommerce/src/pages/mainPage.ts
@@ -1,5 +1,6 @@
 import { mainSlider } from '../components/mainSlider/mainSlider';
 import './mainPage.sass';
+import '../assets/images/sale.png';
 
 const mainPage = document.createElement('div');
 const info = document.createElement('div');
@@ -7,6 +8,9 @@ info.classList.add('main-info');
 info.innerHTML = `
   <h2>FAVORITES PLANTS FOR YOUR HOME</h2>
   <p>Plants are the eukaryotes that form the kingdom Plantae; they are predominantly photosynthetic. This means that they obtain their energy from sunlight, using chloroplasts derived from endosymbiosis with cyanobacteria to produce sugars from carbon dioxide and water, using the green pigment chlorophyll. Exceptions are parasitic plants that have lost the genes for chlorophyll and photosynthesis, and obtain their energy from other plants or fungi.</p>  
+  <div class="promo-box">
+    <img class="promo-box__image" src="sale.png" alt="Promo">
+  </div>
 `;
 
 mainPage.append(mainSlider, info);
